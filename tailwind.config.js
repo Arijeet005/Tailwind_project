43550945c06f21c5600844e33d2f1f141df9be0b
Arijeet005 <arijeet919@gmail.com>
2024-07-14 18:16:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+      extend: {
+        colors: {
+          'primary': '#3238f2',
+        },
+        fontFamily: {
+          'display': ['Poppins', 'sans-serif'],
+          'body': ['Inter', 'sans-serif']
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
